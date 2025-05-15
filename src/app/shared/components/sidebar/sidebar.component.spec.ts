d3aa967b1f249750bd85cb69ffac05a13cd37350
kingnoob3605 <kingnoob360pro@gmail.com>
2025-05-16 00:07:13 +0800

    SIDEBAR-FIX

--- src/app/shared/components/sidebar/sidebar.component.spec.ts
+++ src/app/shared/components/sidebar/sidebar.component.spec.ts
@@ -1,23 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, inject, signal, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectSidebarStatus } from '../../../store/sidebar/sidebar.selector';
+import { CommonModule } from '@angular/common';
+import { SidebarStatus } from '../../../store/sidebar/sidebar.state';
+import { breakpoints } from '../../utils/breakpoints';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import {
+  collapseSidebar,
+  expandSidebar,
+  hideSidebar,
+} from '../../../store/sidebar/sidebar.actions';
+import { Subject, takeUntil } from 'rxjs';
+import { OutsideClickDirective } from '../../directives/outside-click.directive';
+import { Router } from '@angular/router';
 
-import { SidebarComponent } from './sidebar.component';
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, OutsideClickDirective],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent implements OnDestroy {
+  private store = inject(Store);
+  private breakpointObserver = inject(BreakpointObserver);
+  private destroy$ = new Subject<void>();
+  public router = inject(Router);
 
-describe('SidebarComponent', () => {
-  let component: SidebarComponent;
-  let fixture: ComponentFixture<SidebarComponent>;
+  SidebarStatus = SidebarStatus;
+  sidebarStatus = this.store.selectSignal(selectSidebarStatus);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SidebarComponent]
-    })
-    .compileComponents();
+  isHandset = signal(false);
 
-    fixture = TestBed.createComponent(SidebarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  constructor() {
+    this.breakpointObserver
+      .observe([
+        breakpoints.sm,
+        breakpoints.md,
+        breakpoints.lg,
+        breakpoints.xl,
+        breakpoints.xxl,
+      ])
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((result) => {
+        const isSm = result.breakpoints[breakpoints.sm];
+        const isMd = result.breakpoints[breakpoints.md];
+        const isLg = result.breakpoints[breakpoints.lg];
+        const isXl = result.breakpoints[breakpoints.xl];
+        const isXxl = result.breakpoints[breakpoints.xxl];
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+        if (isMd || isSm) {
+          this.isHandset.set(true);
+          this.store.dispatch(hideSidebar());
+        } else if (isLg) {
+          this.isHandset.set(false);
+          this.store.dispatch(collapseSidebar());
+        } else if (isXl || isXxl) {
+          this.isHandset.set(false);
+          this.store.dispatch(expandSidebar());
+        }
+      });
+  }
+
+  hideSidebar() {
+    if (this.sidebarStatus() !== SidebarStatus.hidden && this.isHandset()) {
+      this.store.dispatch(hideSidebar());
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
